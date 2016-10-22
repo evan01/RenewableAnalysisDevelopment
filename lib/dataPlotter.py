45e4f9652b502d77b4b262d2ec8ed684b4dbf466
evan01 <eknoxmobile@gmail.com>
2016-10-22 10:50:50 -0400

    Got some data!!! Woot!

--- lib/dataPlotter.py
+++ lib/dataPlotter.py
@@ -3,6 +3,9 @@
 '''
 import matplotlib.pyplot as plt
 import time
+from tqdm import tqdm
+import lib.csvImporter as csvImporter
+import lib.rampCalculator as r
 
 
 def plotRampData():
@@ -19,11 +22,45 @@ def savePlot(plt):
     saveFileName = currentDate + "_" + currentTime
     plt.savefig("./plots/"+saveFileName+".png")
 
-def main():
-    plt.plot([1,2,3,4],[1,2,3,4],'ro')
-    plt.axis([0,6,0,20])
-    plt.ylabel("Numbers")
+
+def plotCapacityRamp(data):
+
+    '''
+    class entry:
+    Date = ""
+    Time = ""
+    WindGenBPAControl = ""
+    firstDerivative = 0
+    secondDerivative = 0
+    '''
+
+    #First make a list of the Ramp data to go on the y axis
+    # Then make a list of the capacity data to go onto the x axis
+    ramp = []
+    capacity = []
+    for entry in tqdm(data,desc="Converting data"):
+        ramp.append(entry.firstDerivative)
+        capacity.append(entry.WindGenBPAControl)
+
+    maxRampVal = max(ramp)
+    minRampVal = min(ramp)
+    maxCapVal = max(capacity)
+    minCapVal = min(capacity)
+
+
+    plt.plot(capacity,ramp,"ro")
+    plt.axis([minCapVal,maxCapVal,minRampVal,maxRampVal])
+
     savePlot(plt)
+
+    #Then figure out the axis size
+    print("Hey ")
+
+def main():
+    data = csvImporter.readCsv("./data/WindGenTotalLoadYTD_2016.csv")
+    data = r.getFirstDerivative(data)
+    plotCapacityRamp(data)
+    print("e")
 
 if __name__ == '__main__':
     main()
