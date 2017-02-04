1026bdc3ee262d5e5e25240067da9838ac8e3953
JSINK\Julia <julia.sinkovits21@gmail.com>
2017-02-04 15:30:06 -0500

    My latest changes

--- Application/static/scripts/StatGen.js
+++ Application/static/scripts/StatGen.js
@@ -1,3 +1,50 @@
 function sendData() {
     console.log("Sup evan")
 }
+
+$scope.uploadFile = function(element){
+	$scope.$apply(function($scope){
+		$scope.files = element.files;
+	});
+}
+
+$scope.addFile = function(){
+	UploadService.uploadfile($scope.files,
+		function(msg) //success
+		{
+			console.log('uploaded');
+		},
+		function(msg) //error
+		{
+			console.log('error');
+		});
+}
+
+uploadfile: function (files, success,error){
+	
+	var url = 'file:///C:/Users/Julia/Documents/RenewableAnalysisDevelopment/Application/static/index.html';
+
+	for(var i = 0; i < files.length; i++)
+	{
+		var fd = new FormData();
+
+		fd.append("file",, files[i]);
+		$http.post(url, fd, {
+
+			withCredentials : false,
+
+			headers : {
+				'Content-Type' : undefined
+			},
+			transforRequest : angular.identity
+		})
+		.success(function(data)
+		{
+			console.log(data);
+		})
+		.error(function(data))
+		{
+			console.log(data);
+		});
+	}
+}
